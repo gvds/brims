f7740828c884669b2fbe5c7f4682a95f7d916ee6
Gian van der Spuy <gvds@sun.ac.za>
2025-08-18 15:55:32 +0200

    minor changes

--- tests/Feature/Livewire/SetNewAccountPasswordTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Livewire\SetNewAccountPassword;
-use Livewire\Livewire;
-
-it('renders successfully', function () {
-    Livewire::test(SetNewAccountPassword::class)
-        ->assertStatus(200);
-});
