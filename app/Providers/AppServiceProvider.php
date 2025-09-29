c3aa844ab42de06af29e6023e78801ce57b9bea0
Gian van der Spuy <gvds@sun.ac.za>
2025-09-29 15:37:04 +0200

    telescope installed and working

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,10 @@ class AppServiceProvider extends ServiceProvider
     #[\Override]
     public function register(): void
     {
-        //
+        if ($this->app->environment('local') && class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
+            // $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 
     /**
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AppPanelProvider::class,
     App\Providers\Filament\ProjectPanelProvider::class,
+    // App\Providers\TelescopeServiceProvider::class,
 ];
