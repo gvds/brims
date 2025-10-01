f9145b775bd6c1bc3e5d0e8ee2dee8ee8d2fe722
Gian van der Spuy <gvds@sun.ac.za>
2025-10-01 09:55:36 +0200

    removed project link from topbar blade

--- resources/views/filament/topbar.blade.php
+++ resources/views/filament/topbar.blade.php
@@ -1,3 +1,3 @@
 <div class='font-bold ml-20'>
-    <div class='text-xl'>Current project: <span class='text-sky-500 italic'><a href='/projects/{{$currentProject?->id}}'>{{$currentProject?->title ?? 'None selected'}}</a></span></div>
+    <div class='text-xl'>Current project: <span class='text-sky-500 italic'>{{$currentProject?->title ?? 'None selected'}}</span></div>
 </div>
