ecfa1e97891b25cf65d0e898841e1fd6535612e8
Gian van der Spuy <gvds@sun.ac.za>
2026-01-29 15:46:54 +0200

    fix project leader change bug

--- app/Filament/Resources/Projects/Pages/ListProjects.php
+++ app/Filament/Resources/Projects/Pages/ListProjects.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\Projects\Pages;
 
 use App\Filament\Resources\Projects\ProjectResource;
-use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListProjects extends ListRecords
