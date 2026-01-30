b572caa75ae760070150332e949ff4933fa69203
Gian van der Spuy <gvds@sun.ac.za>
2026-01-30 13:30:13 +0200

    non-enrolled subject can't be edited from view page

--- app/Filament/Project/Resources/Subjects/Pages/ViewSubject.php
+++ app/Filament/Project/Resources/Subjects/Pages/ViewSubject.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Project\Resources\Subjects\Pages;
 
+use App\Enums\SubjectStatus;
 use App\Filament\Project\Resources\Subjects\SubjectResource;
 use Filament\Actions\EditAction;
 use Filament\Resources\Pages\ViewRecord;
@@ -13,7 +14,8 @@ class ViewSubject extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
-            EditAction::make(),
+            EditAction::make()
+                ->visible(fn($record): bool => $record->status !== SubjectStatus::Generated),
         ];
     }
 
