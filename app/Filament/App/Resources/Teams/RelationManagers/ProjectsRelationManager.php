fce38cc04786fd9e200d03e3492b16fdb85a2bc9
Gian van der Spuy <gvds@sun.ac.za>
2026-09-02 13:20:44 +0200

    allow numbers in subjectID prefix

--- app/Filament/App/Resources/Teams/RelationManagers/ProjectsRelationManager.php
+++ app/Filament/App/Resources/Teams/RelationManagers/ProjectsRelationManager.php
@@ -76,11 +76,11 @@ class ProjectsRelationManager extends RelationManager
                     ->schema([
                         TextInput::make('subjectID_prefix')
                             ->label('Prefix')
-                            ->hint('Between 2 and 10 uppercase characters')
+                            ->hint('Between 2 and 10 uppercase characters or numbers')
                             ->required()
                             ->maxLength(10)
                             ->minLength(2)
-                            ->regex('/^[A-Z]{2,10}$/'),
+                            ->regex('/^[A-Z][A-Z0-9]{1,9}$/'),
                         TextInput::make('subjectID_digits')
                             ->label('Digits')
                             ->numeric()
